6f42f29fa5c9a2987f5bc25484095c1a91983959
99yen <kyuuzyuu9yen@gmail.com>
2016-10-04 22:25:55 +0900

    init

---
+++ CommandOnLan/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace CommandOnLan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.バージョン情報ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CommandOnLan";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.バージョン情報ToolStripMenuItem,
+            this.終了ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(186, 60);
+            // 
+            // バージョン情報ToolStripMenuItem
+            // 
+            this.バージョン情報ToolStripMenuItem.Name = "バージョン情報ToolStripMenuItem";
+            this.バージョン情報ToolStripMenuItem.Size = new System.Drawing.Size(185, 28);
+            this.バージョン情報ToolStripMenuItem.Text = "バージョン情報";
+            this.バージョン情報ToolStripMenuItem.Click += new System.EventHandler(this.バージョン情報ToolStripMenuItem_Click);
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(185, 28);
+            this.終了ToolStripMenuItem.Text = "終了";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(144, 0);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "CommandOnLan";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem バージョン情報ToolStripMenuItem;
+
+
+    }
+}
+
---
+++ CommandOnLan/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using Ini;
+
+namespace CommandOnLan
+{
+    public partial class Form1 : Form
+    {
+        const string INI_PATH = @".\col.ini";
+        readonly char[] delimiterChars = {':', '-'};
+        
+        private UdpClient[] objSck;
+        private IniFile ini;
+        private int cmdNum;
+        private int[] port;
+        private string[] cmd;
+        private byte[] macAddr = new byte[6];
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSetting();
+        }
+
+        // 初期化
+        private void InitializeSetting()
+        {
+            ReadSetting();
+            InitializeUdpClient();
+        }
+
+        // 項目設定を読み込む
+        private void ReadSetting()
+        {
+            ini = new IniFile(INI_PATH);
+            cmdNum = int.Parse(ini.IniReadValue("main", "num"));
+
+            port = new int[cmdNum];
+            cmd = new string[cmdNum];
+            for (int i = 0; i < cmdNum; i++)
+            {
+                port[i] = int.Parse(ini.IniReadValue("command" + (i+1).ToString(), "port"));
+                cmd[i]  = ini.IniReadValue("command" + (i+1).ToString(), "command");
+            }
+            
+            // 受信MACアドレス設定
+            string[] macTmp = (ini.IniReadValue("main", "mac")).Split(delimiterChars);
+            // 整数に直す
+            for(int i=0; i<6; i++)
+            {
+                macAddr[i] = Convert.ToByte(macTmp[i], 16);
+            }
+        }
+
+        // UdpClientを設定
+        private void InitializeUdpClient()
+        {
+            objSck = new UdpClient[cmdNum];
+            for(int i=0; i<cmdNum; i++)
+            {
+                objSck[i] = new UdpClient(port[i]);
+                objSck[i].BeginReceive(ReceiveCallback, objSck[i]);
+            }
+        }
+
+        // コマンド実行
+        private void execCmd(int resPort)
+        {
+            Process.Start(cmd[Array.IndexOf(port, resPort)]);
+        }
+
+        // マジックパケットをチェック
+        bool checkMagicPacket(byte[] packet)
+        {
+            // 102バイト未満のマジックパケットは存在しない
+            if (packet.Length < 102)
+            {
+                return false;
+            }
+            // FF:FF:FF:FF:FF:FFから始まっているか？
+            for (int i = 0; i < 6; i++)
+            {
+                if (packet[i] != 0xFF)
+                {
+                    return false;
+                }
+            }
+            // MACアドレスが16回連続で入っているか？
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (packet[6 * i + j + 6] != macAddr[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        // 受信コールバック
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            // ソケット受信
+            IPEndPoint ipAny = new IPEndPoint(IPAddress.Any, 0);
+            byte[] dat = ((UdpClient)(AR.AsyncState)).EndReceive(AR, ref ipAny);
+            int resPort = ((IPEndPoint)(((UdpClient)(AR.AsyncState)).Client.LocalEndPoint)).Port;
+            // コマンド起動
+            if (checkMagicPacket(dat))
+            {
+                execCmd(resPort);
+            }
+            // コールバック再設定
+            ((UdpClient)(AR.AsyncState)).BeginReceive(ReceiveCallback, AR.AsyncState);
+        }
+
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void バージョン情報ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CommandOnLan ver.0.1 (C) 2012 99yen.", "バージョン情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
